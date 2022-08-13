2c73861283e552f0e0d98dc78944ce50789e20a7
Anup <anuppoudel60@gmail.com>
2022-08-13 21:31:52 +1000

    register done

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,12 @@
+const postsResolvers = require("./post");
+
+const userResolvers = require("./users");
+
+module.exports = {
+  Query: {
+    ...postsResolvers.Query,
+  },
+  Mutation: {
+    ...userResolvers.Mutation,
+  },
+};
--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
-const gql = require("graphql-tag");
 
-const Post = require("./models/Post");
-
+const typeDefs = require("./graphql/typeDefs");
 const { MONGODB } = require("./config.js");
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const post = await Post.find();
-        return post;
-      } catch (error) {
-        throw new Error(error);
-      }
-    },
-  },
-};
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
   typeDefs,
