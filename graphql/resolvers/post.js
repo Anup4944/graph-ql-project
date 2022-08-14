ecfcb4b2e0376136786302a6a82b73e0056995c2
Anup <anuppoudel60@gmail.com>
2022-08-14 22:13:24 +1000

    create and delete done

--- graphql/resolvers/post.js
+++ graphql/resolvers/post.js
@@ -1,11 +1,54 @@
 const Post = require("../../models/Post");
+const auth = require("../../utils/auth");
+const { AuthenticationError } = require("apollo-server");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const post = await Post.find();
+        const post = await Post.find().sort({ createdAt: -1 });
         return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+
+        return post;
+      } catch (err) {
+        throw new Error("Post not found");
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = auth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = auth(context);
+
+      try {
+        const post = await Post.findById(postId);
+
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted";
+        } else {
+          throw new AuthenticationError("You can only deleted your post");
+        }
       } catch (error) {
         throw new Error(error);
       }
